1a0a2d5804a94faa6d4cc6f8fb220f1ce8499b32
Yuta Kawabe <yuyu3165@gmail.com>
2013-02-18 18:08:28 +0900

    move files
    move files

--- prac/server.js
+++ prac/server.js
@@ -4,11 +4,8 @@ var url = require("url");
 function start(route, handle) {
   http.createServer( function(request, response) {
     var pathname = url.parse(request.url).pathname;
-    route(handle, pathname);
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write("Hello World");
-    response.end();
-      }).listen(8888);
+    route(handle, pathname, response);
+			}).listen(8888);
 }
 
 exports.start = start;
--- router.js
+++
@@ -1,9 +0,0 @@
-function route(handle, pathname) {
-		if(typeof handle[pathname] === 'function') {
-				handle[pathname]();
-		} else {
-				console.log("no request handler");
-		}
-}
-
-exports.route = route;
