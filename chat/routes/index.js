86fe5684519e2e15c6302e73deb888b89b6e246c
Yuta Kawabe <yuyu3165@gmail.com>
2013-03-22 16:09:18 +0900

    move functions to routes

--- chat/routes/index.js
+++ chat/routes/index.js
@@ -2,15 +2,103 @@
 /*
  * GET home page.
  */
+var app = require('../app.js');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    if(req.session.name) {
+        res.redirect('/lobby');
+    } else {
+        res.render('login', {message: 'input id & pass'});
+    }
 };
 
 exports.login = function(req, res){
-		res.render('login', {message: 'input id & pass'});
+    res.render('login', {message: 'input id & pass'});
 };
 
 exports.signup = function(req, res){
-		res.render('signup', {message: 'input new account information'});
+    res.render('signup', {message: 'input new account information'});
+};
+
+exports.auth = function(req, res){
+    var requser = {userid: req.param('userid'), pass: req.param('pass')};
+    app.UserModel.findOne(requser, function(err, doc) {
+        if(doc) {
+            req.session.name = req.param('userid');
+            res.redirect('/lobby');
+        } else {
+            res.render('login', {message: 'invalid id or pass'});
+        }
+    });
+};
+
+exports.regist = function(req, res){
+    app.UserModel.findOne({userid: req.param('userid')}, function(err, doc){
+        if(doc) {
+            res.render('signup', {message: 'userid already used'});
+        } else {
+            var user = new UserModel();
+            user.userid = req.param('userid');
+            user.pass = req.param('pass');
+            user.state = [];
+            user.save();
+            req.session.name = user.userid;
+            res.redirect('/lobby');
+        }
+    });
+};
+
+exports.logout = function(req, res){
+    delete req.session.name;
+    res.redirect('/login');
+};
+
+exports.lobby = function(req, res){
+    RoomModel.find(function(err, items){
+        if(err) {
+            console.log(err);
+        } else {
+            res.render('lobby', {rooms: items, message: 'choose or create room'});
+        }
+    });
+};
+
+exports.createRoom =  function(req, res){
+    if(req.body.name==null) {
+        res.redirect('/lobby');
+    } else {
+        RoomModel.findOne({name: req.body.name}, function(err, doc){
+            if(err) {
+                console.log(err);
+            } else if(doc) {
+                RoomModel.find(function(err, items){
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        res.render('lobby', {rooms: items, message: 'input name already used'});
+                    }
+                });
+            } else {
+                var room = new RoomModel();
+                room.name = req.body.name;
+                room.owner = req.session.name;
+                room.createDate = new Date();
+                room.save();
+                res.redirect('/lobby');
+            }
+        });
+    }
+};
+
+exports.room = function(req, res){
+    RoomModel.findOne({name: req.params.id}, function(err, doc){
+        if(err) {
+            console.log(err);
+        } else if(doc) {
+            req.session.room = req.params.id;
+            res.render('chat', {name: req.session.room});
+        } else {
+            res.send('not exist room');
+        }
+    });
 };
